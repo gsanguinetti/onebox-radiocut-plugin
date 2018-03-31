69de52d2256185fb2d50154e5f5f6fe852857fa7
Gaston Sanguinetti <gaston.a.sanguinetti@gmail.com>
2018-03-31 13:36:18 -0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,29 +2,12 @@
 # version: 0.1
 # authors: gsanguinetti
 
-Onebox = Onebox
+class Onebox::Engine::OneboxRadiocutPlugin
+  include Onebox::Engine
+  
+  matches_regexp(/^(https?:\/\/)(radiocut.fm\/)(.)+\/?$/)
 
-module Onebox
-  module Engine
-    class OneboxRadiocutPlugin
-      include Engine
-      
-      matches_regexp(/^(https?:\/\/)(radiocut.fm\/)(.)+\/?$/)
-
-      def to_html
-        widget_url = widget_url.gsub('https://','')
-        "<iframe width='100%' height='320px' scrolling='no' frameborder='no' src='#{widget_url}' ></iframe>"
-      rescue
-        @url
-      end
-      
-      # Placeholder is called at each interaction with editor, so do something less iframey
-      def placeholder_html        
-        widget_url = widget_url.gsub('https://','')
-        "<div style='width:100%; height:190px; display:block; background-color:black; color:white;'><div style='padding:10px;'><h2>Extracto Radiocut: #{widget_url}</h2><p>Va a ser reemplazado por el reproductor cuando se publique el post.</p></div></div>"
-      rescue
-        @url
-      end
-    end
+  def to_html
+    "<iframe width='100%' height='320px' scrolling='no' frameborder='no' src='#{@url}' ></iframe>"
   end
 end
