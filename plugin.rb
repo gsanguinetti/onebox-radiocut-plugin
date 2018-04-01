4ca29c833fdbda6ec16f2b20838ed1bacce70c87
Gaston Sanguinetti <gaston.a.sanguinetti@gmail.com>
2018-04-01 18:53:25 -0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,6 +5,10 @@ Onebox = Onebox
 class Onebox::Engine::OneboxRadiocutPlugin
   include Onebox::Engine
   
+  def self.priority
+    0
+  end
+  
   matches_regexp(/https:\/\/radiocut\.fm\/audiocut\/declaracion-de-clemente-cancela-sobre-la-denuncia-a-joe-fernandez/)
 
   def to_html
